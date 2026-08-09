fe53b7d2026d46af8bfec87c90116eb9d9aaeb87
Ravendra Kumar <ravendrakumar20897@gmail.com>
2026-08-10 01:33:31 +0530

    backend_chnaged

--- environment/prepod/backend.tf
+++ environment/prepod/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateaccount12345"
-    container_name       = "tfstate"
-    key                  = "prepod.terraform.tfstate"
+    storage_account_name = "tfstateinfra123456"
+    container_name       = "prepodtfstate"
+    key                  = "preprod.terraform.tfstate"
   }
 }
